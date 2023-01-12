b5e302076b1930c8981fe51e677920968b95423a
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-01-12 10:50:14 +0200

    Feat : Response and Request Object

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,14 +1,26 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import { HttpStatus } from '@nestjs/common';
+import {
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Post,
+  Put,
+  Req,
+  Res,
+} from '@nestjs/common';
 
 @Controller('todo')
 export class TodoController {
   @Get()
-  getTodo() {
-    console.log('Voici les todos ');
+  @HttpCode(202)
+  getTodo(@Req() request: Request, @Res() response: Response) {
+    response.json();
     return 'To do trouves';
   }
 
   @Post()
+  @HttpCode(201)
   addTodo() {
     console.log('Todo ajoute avec succe');
     return 'Todo a ete ajoute';
