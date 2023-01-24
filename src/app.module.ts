9e7294848cfb42d2333349890c343a9fc78616ea
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-01-24 16:37:30 +0200

    add middleare

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,23 @@
-import { Module } from '@nestjs/common';
+import {
+  MiddlewareConsumer,
+  Module,
+  NestMiddleware,
+  NestModule,
+} from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { SecondModule } from './second/second.module';
 import { TodoController } from './todo/todo.controller';
 import { TodoService } from './todo/todo.service';
+import { FirstMiddleware } from './middlewares/first/first.middleware';
 
 @Module({
   imports: [SecondModule],
   controllers: [AppController, TodoController],
   providers: [AppService, TodoService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer.apply(FirstMiddleware).forRoutes('todo');
+  }
+}
