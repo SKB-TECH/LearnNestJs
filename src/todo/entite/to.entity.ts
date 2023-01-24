33317046f5419661acf75eab96f9eb5fc02364b1
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-01-24 14:18:35 +0200

    Feat add class-validator

--- src/todo/entite/to.entity.ts
+++ src/todo/entite/to.entity.ts
@@ -1,7 +1,39 @@
+import {
+  IsIn,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  MAX_LENGTH,
+  Max,
+  MaxLength,
+  isIn,
+  isNumber,
+  max,
+  maxLength,
+} from 'class-validator';
+
 //une image de la classe to do
 export class Todo {
+  @IsNotEmpty()
+  @IsNumber()
   id: number;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
   createdAd: Date;
+
+  // @IsNotEmpty()
+  // @IsIn(['a', 'b'])
+  // status;
+  // @MaxLength(12, {
+  //   message: 'La taille doit etre superieur a 13',
+  // })
+  // view;
 }
