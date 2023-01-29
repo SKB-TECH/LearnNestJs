3300fd4e526ea56ec94ccd2387c4acae0d658afa
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-01-30 01:24:23 +0200

    eat generate password with bcrypt

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,14 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Injectable, Post } from '@nestjs/common';
+import { UserService } from './user.service';
+import { UserEntity } from 'src/user/entites/user.entity/user.entity';
+import { subscribeDTO } from './dto/subscribeDTO';
 
 @Controller('user')
-export class UserController {}
+export class UserController {
+  constructor(private UserService: UserService) {}
+
+  @Post('register')
+  async register(@Body() userData: subscribeDTO): Promise<UserEntity> {
+    return await this.UserService.register(userData);
+  }
+}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,4 +1,32 @@
-import { Injectable } from '@nestjs/common';
+import { ConflictException, Injectable } from '@nestjs/common';
+import { subscribeDTO } from './dto/subscribeDTO';
+import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entites/user.entity/user.entity';
+import { Repository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import * as bycript from 'bcrypt';
 
 @Injectable()
-export class UserService {}
+export class UserService {
+  constructor(
+    @InjectRepository(UserEntity)
+    private userRepository: Repository<UserEntity>,
+  ) {}
+  async register(userData: subscribeDTO): Promise<UserEntity> {
+    const user = await this.userRepository.create({
+      ...userData,
+    });
+
+    user.salt = await bycript.genSalt();
+    user.password = await bycript.hash(user.password, user.salt);
+
+    try {
+      this.userRepository.save(user);
+    } catch (error) {
+      throw new ConflictException(
+        `Les informations user doivent etre unique ${error}`,
+      );
+    }
+    return null;
+  }
+}
