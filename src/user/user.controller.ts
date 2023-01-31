015ffd888db31114c8b454c9565c4a1569988dea
Shako Kinyamba <shakokinyamba201@gmail.com>
2023-01-31 11:38:17 +0200

    Feat add login service

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -2,13 +2,21 @@ import { Body, Controller, Injectable, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserEntity } from 'src/user/entites/user.entity/user.entity';
 import { subscribeDTO } from './dto/subscribeDTO';
+import { CredentialDto } from './dto/creadentialDto';
 
 @Controller('user')
 export class UserController {
   constructor(private UserService: UserService) {}
 
   @Post('register')
-  async register(@Body() userData: subscribeDTO): Promise<UserEntity> {
+  async register(@Body() userData: subscribeDTO): Promise<Partial<UserEntity>> {
     return await this.UserService.register(userData);
+  }
+
+  @Post('login')
+  async login(
+    @Body() credentialDto: CredentialDto,
+  ): Promise<Partial<UserEntity>> {
+    return await this.UserService.login(credentialDto);
   }
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,10 +1,15 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { subscribeDTO } from './dto/subscribeDTO';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/user/entites/user.entity/user.entity';
 import { Repository } from 'typeorm';
-import * as bcrypt from 'bcrypt';
 import * as bycript from 'bcrypt';
+import { CredentialDto } from './dto/creadentialDto';
+import { throwError } from 'rxjs';
 
 @Injectable()
 export class UserService {
@@ -12,7 +17,7 @@ export class UserService {
     @InjectRepository(UserEntity)
     private userRepository: Repository<UserEntity>,
   ) {}
-  async register(userData: subscribeDTO): Promise<UserEntity> {
+  async register(userData: subscribeDTO): Promise<Partial<UserEntity>> {
     const user = await this.userRepository.create({
       ...userData,
     });
@@ -21,12 +26,41 @@ export class UserService {
     user.password = await bycript.hash(user.password, user.salt);
 
     try {
-      this.userRepository.save(user);
+      await this.userRepository.save(user);
     } catch (error) {
       throw new ConflictException(
         `Les informations user doivent etre unique ${error}`,
       );
     }
-    return null;
+    return {
+      id: user.id,
+      username: user.username,
+      email: user.email,
+      password: user.password,
+    };
+  }
+
+  async login(credential: CredentialDto): Promise<Partial<UserEntity>> {
+    const { email, password } = credential;
+    const user = await this.userRepository
+      .createQueryBuilder('user')
+      .where('user.email= :email or user.password= :password')
+      .setParameters({ email, password })
+      .getOne();
+
+    if (!user) {
+      throw new NotFoundException('email or password is not correct !!!');
+    }
+    console.log(user);
+    const isMatch = await bycript.compare(password, (await user).password);
+    if (isMatch) {
+      return {
+        username: (await user).username,
+        email: (await user).email,
+        role: (await user).role,
+      };
+    } else {
+      throw new NotFoundException('email or password is not correct !!!');
+    }
   }
 }
